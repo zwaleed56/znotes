512d2d4723cb1a2f7afbe4281790ca32fa9b5d86
Ziad Waleed <zwaleed56@gmail.com>
2025-03-04 00:38:14 +0200

    generate note_model.g.dart

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class NoteModel  {
-  NoteModel({this.title,this.content,this.date,this.color});
-  String? title;
-  String? content;
-  String? date;
-  int? color;
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  NoteModel({required this.title, required this.content, required this.date, required this.color});
+  @HiveField(0)
+ final String title;
+  @HiveField(1)
+ final String content;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int color;
 }
